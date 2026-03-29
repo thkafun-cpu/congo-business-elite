128e3766de5acff7a6f0cbeae1bc60d1e4a3a59e
thkafun-cpu <th.kafun@gmail.com>
2026-03-30 00:46:23 +0200

    Update data_service.py

--- data_service.py
+++ data_service.py
@@ -1,36 +1,10 @@
-# data_service.py
 class DataService:
     @staticmethod
-    def obtenir_entreprises():
-        # Cette liste pourra être remplacée par une base de données ou un appel API
-        return [
-            {
-                "id": "1",
-                "nom": "AgriCongo Solutions",
-                "secteur": "Agriculture",
-                "ville": "Brazzaville",
-                "img": "https://unsplash.com"
-            },
-            {
-                "id": "2",
-                "nom": "Atelier Métal Pro",
-                "secteur": "Industrie",
-                "ville": "Pointe-Noire",
-                "img": "https://unsplash.com"
-            },
-            {
-                "id": "3",
-                "nom": "Congo Services Plus",
-                "secteur": "Services",
-                "ville": "Kinshasa",
-                "img": "https://unsplash.com"
-            }
-        ]
+    def get_businesses():
+        return
 
     @staticmethod
-    def obtenir_actualites():
+    def get_news():
         return [
-            {"id": "1", "titre": "Sommet de l'Élite 2026", "date": "25 Mars", "tag": "Événement"},
-            {"id": "2", "titre": "Transformation Numérique PME", "date": "12 Avril", "tag": "Innovation"}
+            {"id": "1", "title": "Forum 2026", "date": "25 Mars"}
         ]
-
